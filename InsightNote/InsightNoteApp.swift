dfe62e7b37e4ed9f9b76fdfa7f3fc02512e5ce3d
Elraz <elrazh322@gmail.com>
2025-04-03 23:27:22 +0300

    Initial Commit

---
+++ InsightNote/InsightNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  InsightNoteApp.swift
+//  InsightNote
+//
+//  Created by Elraz Hamtsani on 03/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct InsightNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InsightNoteTests/InsightNoteTests.swift
@@ -0,0 +1,17 @@
+//
+//  InsightNoteTests.swift
+//  InsightNoteTests
+//
+//  Created by Elraz Hamtsani on 03/04/2025.
+//
+
+import Testing
+@testable import InsightNote
+
+struct InsightNoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
